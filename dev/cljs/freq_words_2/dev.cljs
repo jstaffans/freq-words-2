015617e868fe48285535c13d5fe118520c529a9a
jstaffans <johannes.staffans@gmail.com>
2016-02-10 00:20:24 +0100

    Add devtools

--- dev/cljs/freq_words_2/dev.cljs
+++ dev/cljs/freq_words_2/dev.cljs
@@ -1,8 +1,11 @@
  (ns ^:figwheel-no-load freq-words-2.dev
    (:require [figwheel.client :as figwheel :include-macros true]
+             [devtools.core :as devtools]
              [freq-words-2.core :refer [mount-root]]))
 
 (figwheel/watch-and-reload
    :websocket-url "ws://localhost:3449/figwheel-ws"
    :jsload-callback mount-root)
 
+(devtools/enable-feature! :sanity-hints :dirac)
+(devtools/install!)
